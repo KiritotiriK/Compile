306eebbc8f65d7a62aeec1ba2ef203bd5ccba4eb
KiritotiriK <89820065+KiritotiriK@users.noreply.github.com>
2022-06-08 23:57:16 +0800

    Add files via upload

---
+++ 6.7/test/abs.c
@@ -0,0 +1,10 @@
+void main(int n ) {
+
+    if(abs(n)==8){
+     print 1;
+    }
+    else
+    {
+     print 0;
+    }
+}
---
+++ 6.7/test/max.c
@@ -0,0 +1,10 @@
+void main(int n ) {
+
+    if(max(n,7)==8){
+     print 1;
+    }
+    else
+    {
+     print 0;
+    }
+}
